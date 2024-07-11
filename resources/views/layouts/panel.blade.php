61eb28fb815ecc88daac1e81870ba6eb29aa6901
Erick <202100366a@utea.edu.pe>
2024-07-10 19:38:10 -0500

    Prueba

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -42,8 +42,9 @@
 	<!-- [ Header ] start -->
 	<header class="navbar pcoded-header navbar-expand-lg navbar-light header-blue">
 		
-			
 		
+			
+
 				<div class="m-header">
 					<a class="mobile-menu" id="mobile-collapse" href="#!"><span></span></a>
 					<a href="#!" class="b-brand">
