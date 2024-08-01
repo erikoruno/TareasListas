2f6a2c83372db77c53495e8b230fba5813b5c136
Erick <202100366a@utea.edu.pe>
2024-08-01 03:12:29 -0500

    con Seeder automático

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,17 +14,35 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        \App\Models\User::factory()->create([
+      $user1 =  \App\Models\User::factory()->create([
             'name' => 'Erick',
             'lastname' => 'Ergueta Paravicino',
             'email' => 'eround@hotmail.com',
             'password' => bcrypt('12345678'),
+
+           
         ]);
+
+    $user2 =  \App\Models\User::factory()->create([
+        'name' => 'Pepito',
+        'lastname' => 'De los Palos',
+        'email' => 'loki@gmail.com',
+        'password' => bcrypt('12345678'),
+
+    ]);
 
     \App\Models\Tareas::factory()->create([
         'nombreTarea' => 'Exámen',
-        'fechaVencimiento' => '2024/07/21',
+        'fechaVencimiento' => '2024/08/20',
         'prioridad' => '1',
+        'user_id' => $user1->id,
+    ]);
+
+    \App\Models\Tareas::factory()->create([
+        'nombreTarea' => 'Bicicleta',
+        'fechaVencimiento' => '2024/08/21',
+        'prioridad' => '2',
+        'user_id' => $user2->id,
     ]);
     }
 }
